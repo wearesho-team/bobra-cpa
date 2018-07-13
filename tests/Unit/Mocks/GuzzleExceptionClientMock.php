be3851f5116d9efffbfbe9bdf1c573e455311e1a
Alexander Letnikow <reclamme@gmail.com>
2018-07-13 16:43:56 +0300

    Add phpDoc to GuzzleExceptionClientMock

--- tests/Unit/Mocks/GuzzleExceptionClientMock.php
+++ tests/Unit/Mocks/GuzzleExceptionClientMock.php
@@ -5,10 +5,16 @@ namespace Wearesho\Bobra\Cpa\Tests\Unit\Mocks;
 use Psr\Http\Message\RequestInterface;
 use GuzzleHttp\Exception\GuzzleException;
 
+/**
+ * Class GuzzleExceptionClientMock
+ * @package Wearesho\Bobra\Cpa\Tests\Unit\Mocks
+ */
 class GuzzleExceptionClientMock extends ClientMock
 {
     public function send(RequestInterface $request, array $options = [])
     {
-        throw new class extends \Exception implements GuzzleException {};
+        throw new class extends \Exception implements GuzzleException
+        {
+        };
     }
 }
