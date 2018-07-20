edcd716dbde63fc5557fb22d2c2dac89f65018b1
Alexander Letnikow <reclamme@gmail.com>
2018-07-20 18:23:31 +0300

    Add database environment configuration using dotenv

--- tests/bootstrap.php
+++ tests/bootstrap.php
@@ -2,12 +2,16 @@
 
 // phpcs:ignoreFile
 
-defined('YII_DEBUG') || define("YII_DEBUG", true);
-defined('YII_ENV') || define("YII_ENV", "test");
+require_once(dirname(__DIR__) . '/vendor/autoload.php');
+
+$dotEnv = new \Dotenv\Dotenv(dirname(__DIR__));
+$dotEnv->load();
+
+defined('YII_DEBUG') || define("YII_DEBUG", getenv("YII_DEBUG") ?: true);
+defined('YII_ENV') || define("YII_ENV", getenv("YII_ENV") ?: "test");
 
 getenv('DB_PATH') || putenv("DB_PATH=" . __DIR__ . '/db.sqlite');
 
-require_once(dirname(__DIR__) . '/vendor/autoload.php');
 require_once(dirname(__DIR__) . '/vendor/yiisoft/yii2/Yii.php');
 
 Yii::setAlias(
