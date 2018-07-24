2ff3f67074818e31846fb39d6f05367200548007
Alexander Letnikow <reclamme@gmail.com>
2018-07-24 18:43:15 +0300

    Move authenticator behavior after options

--- src/Web/Controller.php
+++ src/Web/Controller.php
@@ -53,9 +53,6 @@ class Controller extends web\Controller
     public function behaviors(): array
     {
         return [
-            'authenticator' => [
-                'class' => filters\auth\HttpBearerAuth::class,
-            ],
             'verb' => [
                 'class' => filters\VerbFilter::class,
                 'actions' => [
@@ -67,6 +64,9 @@ class Controller extends web\Controller
             ],
             'options' => [
                 'class' => OptionsRequestBehavior::class,
+            ],
+            'authenticator' => [
+                'class' => filters\auth\HttpBearerAuth::class,
             ],
             'access' => [
                 'class' => filters\AccessControl::class,
