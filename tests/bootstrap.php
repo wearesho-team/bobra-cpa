e0c565e15b5ff5c3049be4b12837707e51d45808
Alexander Letnikow <reclamme@gmail.com>
2018-07-24 14:08:00 +0300

    Remove excess tests configuration and bootstrap

--- tests/bootstrap.php
+++ tests/bootstrap.php
@@ -10,8 +10,6 @@ $dotEnv->load();
 defined('YII_DEBUG') || define("YII_DEBUG", getenv("YII_DEBUG") ?: true);
 defined('YII_ENV') || define("YII_ENV", getenv("YII_ENV") ?: "test");
 
-getenv('DB_PATH') || putenv("DB_PATH=" . __DIR__ . '/db.sqlite');
-
 require_once(dirname(__DIR__) . '/vendor/yiisoft/yii2/Yii.php');
 
 Yii::setAlias(
@@ -19,8 +17,3 @@ Yii::setAlias(
     dirname(__DIR__) . DIRECTORY_SEPARATOR . 'src'
 );
 Yii::setAlias('@configFile', __DIR__ . DIRECTORY_SEPARATOR . 'config.php');
-
-Yii::$container->set(
-    \GuzzleHttp\ClientInterface::class,
-    \Wearesho\Bobra\Cpa\Tests\Unit\Mocks\ClientMock::class
-);
