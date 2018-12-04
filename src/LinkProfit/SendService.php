8d03ff0a68c0345e0c5edf83566fb4f9ed1442e5
Alexander Yagich <aleksa.yagich@gmail.com>
2018-12-04 17:14:38 +0200

    Implement sending additional request to linkprofit (#27)

--- src/LinkProfit/SendService.php
+++ src/LinkProfit/SendService.php
@@ -3,7 +3,7 @@
 namespace Wearesho\Bobra\Cpa\LinkProfit;
 
 use Psr\Http\Message\RequestInterface;
-use GuzzleHttp\Psr7;
+use GuzzleHttp;
 use Wearesho\Bobra\Cpa;
 use yii\base;
 use yii\di;
@@ -14,11 +14,14 @@ use yii\di;
  */
 class SendService extends base\BaseObject implements Cpa\Conversion\SendServiceInterface
 {
-    use Cpa\Conversion\SendServiceTrait;
-
     /** @var array|string|ConfigInterface definition */
     public $config = [
         'class' => EnvironmentConfig::class,
+    ];
+
+    /** @var array|string|GuzzleHttp\ClientInterface definition */
+    public $client = [
+        'class' => GuzzleHttp\ClientInterface::class,
     ];
 
     /**
@@ -30,7 +33,7 @@ class SendService extends base\BaseObject implements Cpa\Conversion\SendServiceI
         $this->config = di\Instance::ensure($this->config, ConfigInterface::class);
     }
 
-    protected function getRequest(Cpa\ConversionInterface $conversion): RequestInterface
+    protected function getInitialRequest(Cpa\ConversionInterface $conversion): RequestInterface
     {
         $query = [
             'OrderID' => $conversion->getId(),
@@ -39,8 +42,38 @@ class SendService extends base\BaseObject implements Cpa\Conversion\SendServiceI
             'AffiliateID' => $conversion->getConfig()['refId'] ?? '',
         ];
 
-        $url = ConfigInterface::URL . '?' . http_build_query($query);
+        $url = 'https://cpa.linkprofit.ru/sale?' . http_build_query($query);
 
-        return new Psr7\Request("GET", $url);
+        return new GuzzleHttp\Psr7\Request('GET', $url);
+    }
+
+    protected function getAdditionalRequest(Cpa\ConversionInterface $conversion): RequestInterface
+    {
+        $url = "http://s.linkprofit.ru/postback/{$conversion->getProduct()}/update/{$conversion->getId()}?Status=A";
+
+        return new GuzzleHttp\Psr7\Request('GET', $url);
+    }
+
+    /**
+     * @param Cpa\ConversionInterface $conversion
+     * @return Cpa\PostbackTuple
+     * @throws \yii\base\InvalidConfigException
+     */
+    final public function send(Cpa\ConversionInterface $conversion): Cpa\PostbackTuple
+    {
+        /** @var GuzzleHttp\ClientInterface $client */
+        $client = di\Instance::ensure($this->client, GuzzleHttp\ClientInterface::class);
+        $request = $this->getAdditionalRequest($conversion);
+
+        try {
+            $client->send($this->getInitialRequest($conversion));
+            $response = $client->send($request);
+        } catch (GuzzleHttp\Exception\RequestException $e) {
+            return new Cpa\PostbackTuple($request, $e->getResponse());
+        } catch (GuzzleHttp\Exception\GuzzleException $e) {
+            return new Cpa\PostbackTuple($request);
+        }
+
+        return new Cpa\PostbackTuple($request, $response);
     }
 }
