42c3a00b6121d985540aa425438d5ece023cf6f5
Beton <aleksa.yagich@gmail.com>
2018-12-17 16:09:05 +0200

    Refactor query params trait

--- src/Lead/Parser/QueryParams.php
+++ src/Lead/Parser/QueryParams.php
@@ -8,20 +8,15 @@ namespace Wearesho\Bobra\Cpa\Lead\Parser;
  */
 trait QueryParams
 {
+    /**
+     * @param string $url
+     * @return array        Associative array where key is the name of query param and value is its value
+     *                      'https://test.com?a=b&c=d' => ['a' => 'b', 'c' => 'd',]
+     */
     protected function getQueryParams(string $url): array
     {
-        $queryString = parse_url($url)['query'] ?? '';
-        $query = [];
-
-        foreach (explode('&', $queryString) as $paramPair) {
-            $params = explode('=', $paramPair);
-            if (count($params) < 2) {
-                continue;
-            }
-
-            $query[$params[0]] = $params[1];
-        }
-
-        return $query;
+        $queryString = parse_url($url, PHP_URL_QUERY) ?? '';
+        parse_str($queryString, $output);
+        return $output;
     }
 }
